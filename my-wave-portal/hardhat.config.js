a3392b46c481142e4ecf8062cb7c782707954743
MAJEED <majeedyahia.com>
2022-10-04 15:17:25 +0300

    first commit

--- my-wave-portal/hardhat.config.js
+++ my-wave-portal/hardhat.config.js
@@ -1,7 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
-
+require("@nomicfoundation/hardhat-toolbox");
+// Import and configure dotenv
+require("dotenv").config();
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
+
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
@@ -18,4 +20,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  networks:{
+    goerli:{
+      url:"https://proud-yolo-night.ethereum-goerli.discover.quiknode.pro/13301ae87f0af936e6ed14e114cceaac26a786ea/",
+      accounts:["71000f075f5389c33ee2f4e8b0d882b25b00df544eb3e50a4fdc5cb034a6ecca"]
+    },
+    mainnet:{
+      url:"https://sparkling-hardworking-resonance.discover.quiknode.pro/c2f8dfbfbb7901ba432aa3f41eae883e057da922/",
+      accounts:["71000f075f5389c33ee2f4e8b0d882b25b00df544eb3e50a4fdc5cb034a6ecca"]
+    },
+  },
 };
